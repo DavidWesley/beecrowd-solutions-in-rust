e7576ff4cbb6e6a095c6505b81ba916c46a282ce
David Wesley <contato.dwesley@outlook.com>
2025-01-04 16:01:10 -0300

    feat: 1001 - Extremely Basic

--- src/beginner/p1001.rs
+++ src/beginner/p1001.rs
@@ -1,14 +1,14 @@
 use std::io;
 
 pub fn main() {
-    let mut inputA = String::new();
-    let mut inputB = String::new();
+    let mut input_a = String::new();
+    let mut input_b = String::new();
 
-    io::stdin().read_line(&mut inputA).unwrap();
-    io::stdin().read_line(&mut inputB).unwrap();
+    io::stdin().read_line(&mut input_a).unwrap();
+    io::stdin().read_line(&mut input_b).unwrap();
 
-    let a: i32 = inputA.trim().parse().unwrap();
-    let b: i32 = inputB.trim().parse().unwrap();
+    let a: i32 = input_a.trim().parse().unwrap();
+    let b: i32 = input_a.trim().parse().unwrap();
 
     println!("X = {}", a + b);
 }
